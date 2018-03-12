de075f25232af3a04e2b78c0b6c3705473f5c91c
Jose Carmona <j.carmona.n@gmail.com>
2018-03-12 07:15:59 +0100

    dashboard histograma con brush

--- apps/tests/SMS_DB/app.R
+++ apps/tests/SMS_DB/app.R
@@ -4,12 +4,13 @@ library(ggplot2)
 library(scales)
 library(dplyr)
 
+
 ui <- dashboardPage(
 
   # Título
   dashboardHeader(title = "Mensajes"),
 
-  # Sidebar content
+  # Sidebar
   dashboardSidebar(
     sidebarMenu(
       menuItem("Mensajes", tabName = "dashboard", icon = icon("envelope")),
@@ -17,14 +18,17 @@ ui <- dashboardPage(
     )
   ),
 
-  # contenido
+  # Contenido
   dashboardBody(
     tabItems(
-      # First tab content
+      # Tab del dashboard
       tabItem(tabName = "dashboard",
         verticalLayout(
           fluidRow(
-            box( title = "Eventos", width = 10, status = "primary", solidHeader = TRUE, collapsible = TRUE, plotOutput("plotM")),
+            # Histograma de Eventos
+            box( title = "Eventos", width = 10, status = "primary", solidHeader = TRUE, collapsible = TRUE, 
+                 plotOutput("plotM", brush = brushOpts(id = "plotM_brush", direction = "x"))),
+            # Selector de Fecha y hora
             column( 2,
               dateInput('fecha',
                         label = 'Fecha',
@@ -32,9 +36,14 @@ ui <- dashboardPage(
                         min = Sys.Date() - 5, max = Sys.Date(),
                         format = "dd/mm/yy",
                         startview = 'month', language = 'es-ES', weekstart = 1
-              ),
-              sliderInput("horas", label = 'Horas', min = 0, max = 24, value = c(9, 21))
+              )
             )
+          ),
+          fluidRow(
+            # Porcentajes de Respuetas Totales
+            box( title = "% Total respuesta", width = 5, status = "primary", solidHeader = TRUE, collapsible = TRUE, plotlyOutput("plotT1")),
+            # Porentajes de Respuetas en el periodo
+            box( title = "% respuesta periodo", width = 5, status = "primary", solidHeader = TRUE, collapsible = TRUE, plotOutput("plotT2"))
           )
         )
       )
@@ -45,22 +54,30 @@ ui <- dashboardPage(
 # Define server logic to display and download selected file ----
 server <- function(input, output) {
 
+  # Leemos los Eventos
   eventos <- read.csv("/data/hist_eventos.csv")
   
+  # Fecha/Hora de Eventos en POSIX
   eventos <- eventos %>% mutate( fh = as.POSIXct(fh_evento, "%Y-%m-%d %H:%M:%S"))
-  
-  
+
+  # Histograma 
   output$plotM <- renderPlot({
     
-    limites <- c(as.POSIXct(paste0(input$fecha," ", input$horas[1], ":00:00"), "%Y-%m-%d %H:%M:%S"), 
-                 as.POSIXct(paste0(input$fecha," ", input$horas[2], ":00:00"), "%Y-%m-%d %H:%M:%S"))
+    if( !is.null(input$plotM_brush) ) {
+      limites <- c(as.POSIXct(input$plotM_brush$xmin, origin = "1970-01-01"), 
+                   as.POSIXct(input$plotM_brush$xmax, origin = "1970-01-01"))
+    } 
+    else {
+      limites <- c(as.POSIXct(paste0(input$fecha," 09:00:00"), "%Y-%m-%d %H:%M:%S"), 
+                   as.POSIXct(paste0(input$fecha," 21:00:00"), "%Y-%m-%d %H:%M:%S"))
+    }
     
     minutos <- as.numeric(difftime(limites[2], limites[1], units="mins"))
     
     ggplot( eventos, aes( x = fh, colour = Evento ) ) +
       geom_freqpoly(bins = minutos) +
       scale_x_datetime(date_minor_breaks = "1 min",
-                       labels = date_format("%H"),
+                       labels = date_format("%H:%M"),
                        limits =  limites ) +
       theme_minimal()
   })
@@ -68,5 +85,5 @@ server <- function(input, output) {
 
 }
 
-# Create Shiny app ----
+# Shiny App
 shinyApp(ui, server)
---
+++ shinydashboard/app.R
@@ -0,0 +1,96 @@
+library(shiny)
+library(shinydashboard)
+library(ggplot2)
+
+ui <- dashboardPage(
+
+  # Título
+  dashboardHeader(title = "Investigaciones",dropdownMenuOutput("messageMenu")),
+
+  # Sidebar content
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+      menuItem("Widgets", tabName = "widgets", icon = icon("th"))
+    )
+  ),
+
+  # contenido
+  dashboardBody(
+    tabItems(
+      # First tab content
+      tabItem(tabName = "dashboard",
+        verticalLayout(
+          fluidRow(
+            infoBoxOutput("infoBox"),
+            valueBoxOutput("valueBox"),
+            downloadButton("downloadData", "Download")),
+          box( title = "Curva", status = "primary", solidHeader = TRUE, collapsible = TRUE, plotOutput("plot")),
+          box( title = "tabla", status = "primary", solidHeader = TRUE, collapsible = TRUE, tableOutput("table"))
+        )
+      )
+    )
+  )
+)
+
+# Define server logic to display and download selected file ----
+server <- function(input, output) {
+
+  # Reactive value for selected dataset ----
+  datasetInput <- read.csv(file="/data/test.csv", header=TRUE, sep=",")
+
+  df1 <- fromJSON(getURL("https://proyectos.eprinsa.es/time_entries.json?limit=200&spent_on=%3E%3C2018-01-01|2018-02-01"))
+  horas <- df1[["time_entries"]][["hours"]]
+  actividades <- df1[["time_entries"]][["activity"]][["name"]]
+  dt_act_h <- data.frame( actividades, horas)
+  sumas <- aggregate(x=dt_act_h$horas, by=list(actividades=dt_act_h$actividades), FUN=sum)
+
+  # Table of selected dataset ----
+  output$table <- renderTable(sumas)
+
+  # Downloadable csv of selected dataset ----
+  output$downloadData <- downloadHandler(
+    filename = function() {
+      paste("nombre", ".csv", sep = "")
+    },
+    content = function(file) {
+      write.csv(datasetInput, file, row.names = FALSE)
+    }
+  )
+
+  output$messageMenu <- renderMenu({
+    dropdownMenu(type = "tasks", badgeStatus = "success",
+      taskItem(value = 90, color = "green", "Documentation"),
+      taskItem(value = 17, color = "aqua", "Project X"),
+      taskItem(value = 75, color = "yellow", "Server deployment"),
+      taskItem(value = 80, color = "red", "Overall project")
+    )
+  })
+
+  output$infoBox <- renderInfoBox({
+    infoBox(
+      "Investigaciones", nrow(datasetInput), icon = icon("flask")
+    )
+  })
+
+  output$valueBox <- renderValueBox({
+    valueBox(
+      sum(datasetInput$num), "Total", icon = icon("list")
+    )
+  })
+
+  # Fill in the spot we created for a plot
+  output$plot <- renderPlot({
+
+    # Render a barplot
+    # barplot(datasetInput$field_dotacion_economica, ylab="ylab", xlab="xlab")
+    ggplot(dt_act_h, aes( x=1, fill=actividades, weight=horas)) + geom_bar() + coord_polar(theta="y")
+
+
+  })
+
+
+}
+
+# Create Shiny app ----
+shinyApp(ui, server)
